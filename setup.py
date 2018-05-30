2a90171dce6b29a411ee9ebd8f052c4e1962b75e
ranulf.green@gmail.com <ranulf.green@gmail.com>
2018-05-30 15:01:15 -0400

    update

--- setup.py
+++ setup.py
@@ -24,12 +24,6 @@ REQUIRED = [
 
 
 
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
